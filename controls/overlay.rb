b7a3b70a1ba0003b3feead9ff3dd96ac7ea92da9
Haynes Jr <dhaynes@mitre.org>
2019-05-09 13:03:48 -0400

    updating overlay based on EA feedback.

--- controls/overlay.rb
+++ controls/overlay.rb
@@ -1,5 +1,24 @@
 # encoding: utf-8
 
-include_controls 'aws-rds-infrastructure-baseline' do
+include_controls 'aws-rds-infrastructure-cis-baseline' do
+  control 'aws-rds-baseline-4' do
+  desc 'rationale', 'Provides a managed backup function of the RDS Database, it 
+       is possible to define the backup window and retention period of the backup. 
+       Each customer should have a retention policy set for the type of data being 
+       stored. Recommend setting this to at least 3'
+  desc 'check', 'Using the Amazon unified command line interface:
+       * Check if your application DB instances have a Backup Retention Period set 
+       (3 = there are 3 daily backups retained):
 
+       aws rds describe-db-instances --filters
+       Name=tag:<data_tier_tag>,Values=<data_tier_tag_value> --query
+       \'DBInstances[*].{BackupRetentionPeriod:BackupRetentionPeriod,
+       DBInstanceIdentifier:DBInstanceIdentifier}\''
+  desc 'fix', 'Using the Amazon unified command line interface:
+       * Modify each DB instance with Backup Retention Period of 0, and set a 
+       desired Backup Retention Period in days (recommended value = 3):
+
+       aws rds modify-db-instance --db-instance-identifier <your_db_instance>
+       --backup- retention-period <backup_retention_period>'
+  end
 end
